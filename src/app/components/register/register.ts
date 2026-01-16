44bd30b07881048410c8996c82e76a5ef865c7f1
Edwin Espinoza <edwin.smx2@gmail.com>
2026-01-16 16:54:36 +0100

    Corregido link Register

--- src/app/components/register/register.ts
+++ src/app/components/register/register.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterLink} from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
